4e6e8b571f1be744d582ea0dcb60103fbdb4e258
Jayce Rocha <jeyce_jp@hotmail.com>
2019-06-05 20:59:04 -0300

    usando o context

--- exeplo_rspec_tdd/spec/calculator/calculator_spec.rb
+++ exeplo_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -1,14 +1,24 @@
 require 'calculator'
 
 describe Calculator do
-  it 'use sum method for 2 numbers' do
-  	result = subject.sum(4,4)
-  	expect(result).to eq(8)	
-  end	
-
-  it 'use multiplication method for 2 numbers' do
-  	result = subject.multiplication(4,4)
-  	expect(result).to eq(16)	
+  context '#sum' do
+    it 'use sum method for 2 numbers' do
+      result = subject.sum(4,4)
+      expect(result).to eq(8) 
+    end 
+    it 'use sum method for 2 numbers negative' do
+      result = subject.sum(4,-2)
+      expect(result).to eq(2) 
+    end
   end
+  
+
+  context '#multiplication' do
+     it 'use multiplication method for 2 numbers' do
+      result = subject.multiplication(4,4)
+      expect(result).to eq(16)  
+    end
+  end
+ 
   
 end
