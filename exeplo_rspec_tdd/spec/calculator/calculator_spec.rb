f939638b580f22f1fb1ee50c3140b44c59c9ef99
Jayce Rocha <jeyce_jp@hotmail.com>
2019-06-05 20:49:49 -0300

    utilizando o subject

--- exeplo_rspec_tdd/spec/calculator/calculator_spec.rb
+++ exeplo_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -2,14 +2,12 @@ require 'calculator'
 
 describe Calculator do
   it 'use sum method for 2 numbers' do
-  	calc = Calculator.new
-  	result = calc.sum(4,4)
+  	result = subject.sum(4,4)
   	expect(result).to eq(8)	
   end	
 
   it 'use multiplication method for 2 numbers' do
-  	calc = Calculator.new
-  	result = calc.multiplication(4,4)
+  	result = subject.multiplication(4,4)
   	expect(result).to eq(16)	
   end
   
